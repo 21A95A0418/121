550daac7bcf22b22b7ca211349a3daa8161172ef
Gopalapurapu Satyanarayana <109515128+21A95A0418@users.noreply.github.com>
2023-04-07 02:37:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.84.0">
-    <title>Sign in Page PHP Demo</title>
+    <title>Sign in Page PHP demo-project</title>
     <!-- Bootstrap core CSS -->
     <link href="https://getbootstrap.com/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- Favicons -->
